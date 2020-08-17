9d2f49156fc2cd857b342eb37b47c760d3be884c
Ryan Altimari <ryanaltimari8@gmail.com>
2020-08-17 12:50:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,4 +7,4 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Ryan, Ruby)
+introduction_with_language("Ryan", "Ruby")
